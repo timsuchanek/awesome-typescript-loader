944434ff2183abbdd2117f1c3cc5bae32e6656cb
Tim Suchanek <tim.suchanek@gmail.com>
2017-01-09 12:55:35 +0100

    add build

---
+++ dist/interfaces.d.ts
@@ -0,0 +1,29 @@
+export interface CompilerInfo {
+    compilerPath: string;
+    compilerVersion: string;
+    tsImpl: typeof ts;
+}
+export interface LoaderConfig {
+    instance?: string;
+    compiler?: string;
+    configFileName?: string;
+    configFileContent?: string;
+    transpileOnly?: boolean;
+    ignoreDiagnostics?: number[];
+    compilerOptions?: ts.CompilerOptions;
+    useTranspileModule?: boolean;
+    useBabel?: boolean;
+    babelCore?: string;
+    babelOptions?: any;
+    usePrecompiledFiles?: boolean;
+    silent?: boolean;
+    useCache?: boolean;
+    cacheDirectory?: string;
+    muteNodeModules?: boolean;
+}
+export interface OutputFile {
+    text: string;
+    sourceMap: string;
+    declaration: ts.OutputFile;
+}
+export declare type TsConfig = ts.ParsedCommandLine;
